66fe848ee75bd6d4ef2096e0e870e340f375a733
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-04-20 11:06:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
     //Write your code here
-
-
+	bold.style.color = green;
 }
 
-
+let bold = document.getElementsByTagName("strong");
+bold.addEventListener("mouseover" , highlight);
+bold.addEventListener("mouseout" ,return_normal);
 function return_normal() {
     //Write your code here
 
-    
+    bold.style.color = black;
 }
